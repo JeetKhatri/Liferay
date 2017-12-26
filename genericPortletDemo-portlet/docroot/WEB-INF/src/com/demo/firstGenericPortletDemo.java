60bb149666bea3a6d9261f111f98705982c28537
Jeet Khatri <jeet.g.khatri@gmail.com>
2017-12-26 17:01:14 +0530

    Till Tutorial 4

---
+++ genericPortletDemo-portlet/docroot/WEB-INF/src/com/demo/firstGenericPortletDemo.java
@@ -0,0 +1,64 @@
+package com.demo;
+
+import java.io.IOException;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.GenericPortlet;
+import javax.portlet.PortletException;
+import javax.portlet.PortletRequestDispatcher;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+
+/**
+ * Portlet implementation class firstGenericPortletDemo
+ */
+public class firstGenericPortletDemo extends GenericPortlet {
+
+    public void init() {
+        viewTemplate = getInitParameter("view-template");
+    }
+
+    public void doView(
+            RenderRequest renderRequest, RenderResponse renderResponse)
+        throws IOException, PortletException {
+
+        include(viewTemplate, renderRequest, renderResponse);
+        System.out.println("view template : "+viewTemplate);
+    }
+
+    
+    @Override
+    public void processAction(ActionRequest request, ActionResponse response) throws PortletException, IOException {
+    	
+    	String name = request.getParameter("name");
+    	String name1= ParamUtil.getString(request, "name");
+    	System.out.println(name+ " "+name1);
+    	
+    }
+    
+    protected void include(
+            String path, RenderRequest renderRequest,
+            RenderResponse renderResponse)
+        throws IOException, PortletException {
+
+        PortletRequestDispatcher portletRequestDispatcher =
+            getPortletContext().getRequestDispatcher(path);
+
+        if (portletRequestDispatcher == null) {
+            _log.error(path + " is not a valid include");
+        }
+        else {
+            portletRequestDispatcher.include(renderRequest, renderResponse);
+        }
+    }
+ 
+    protected String viewTemplate;
+
+    private static Log _log = LogFactoryUtil.getLog(firstGenericPortletDemo.class);
+
+}
