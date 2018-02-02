4d149e425c98c4e30bb82662071789c2e64bdccd
JeetKhatri <jeet.g.khatri@gmail.com>
2018-02-02 17:10:43 +0530

    Dynamic query

--- Service-Builder-MVC-portlet/docroot/WEB-INF/src/com/test/ServiceBuilderUsingMVC.java
+++ Service-Builder-MVC-portlet/docroot/WEB-INF/src/com/test/ServiceBuilderUsingMVC.java
@@ -1,13 +1,19 @@
 package com.test;
 
+import com.liferay.portal.kernel.dao.orm.DynamicQuery;
+import com.liferay.portal.kernel.dao.orm.DynamicQueryFactoryUtil;
+import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
+import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.portlet.PortletClassLoaderUtil;
 import com.liferay.sample.model.Employee;
 import com.liferay.sample.model.impl.EmployeeImpl;
 import com.liferay.sample.service.EmployeeLocalServiceUtil;
 import com.liferay.util.bridges.mvc.MVCPortlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.portlet.PortletException;
 import javax.portlet.RenderRequest;
@@ -29,10 +35,26 @@ public class ServiceBuilderUsingMVC extends MVCPortlet {
     	obj.setEcountry("US");*/
     	try {
 			//EmployeeLocalServiceUtil.addEmployee(obj);
-			System.out.println(EmployeeLocalServiceUtil.getEmployee(1).getEname());
+			//System.out.println(EmployeeLocalServiceUtil.getEmployee(1).getEname());
+    		
 			System.out.println("Total employee : "+EmployeeLocalServiceUtil.getEmployeesCount());
-			System.out.println("Total Indian employee : "+EmployeeLocalServiceUtil.findByEcountry("india").size());
-		} catch (PortalException | SystemException e) {
+			List<Employee> countryEmpList = EmployeeLocalServiceUtil.findByEcountry("india");
+			System.out.println("Total Indian employee : "+countryEmpList.size());
+			
+			
+			//dynamic query - 18
+			DynamicQuery dynamicQuery = DynamicQueryFactoryUtil.forClass(Employee.class, "emp", PortletClassLoaderUtil.getClassLoader());
+			dynamicQuery.add(RestrictionsFactoryUtil.ne("emp.ecountry", "india"));
+												//OR
+			//dynamicQuery.add(PropertyFactoryUtil.forName("emp.ecountry").ne("india"));
+			List<Employee> otherCountryEmpList = EmployeeLocalServiceUtil.dynamicQuery(dynamicQuery); 
+			System.out.println("Other Country's employee : "+otherCountryEmpList.size()+"\nDetails:-");
+			for (Employee employee : otherCountryEmpList) {
+				System.out.println(employee.getEname()+"\t"+employee.getEcountry());
+			}
+			
+			
+		} catch (SystemException e) {
 			e.printStackTrace();
 		}
 		
